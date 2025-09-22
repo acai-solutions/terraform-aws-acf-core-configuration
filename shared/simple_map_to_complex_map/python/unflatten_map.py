02d3c13bcbcb3a08fc1ce0c9b00a0292691d576b
Michael Ullrich <michael.ullrich@acai.gmbh>
2025-09-22 08:43:57 +0200

    fix

--- shared/simple_map_to_complex_map/python/unflatten_map.py
+++ shared/simple_map_to_complex_map/python/unflatten_map.py
@@ -62,7 +62,7 @@ def _ensure_container(parent: NestedType, key: str, next_is_index: bool) -> Nest
     return parent[key]  # type: ignore[index]
 
 
-def _assign(root: NestedType, parts: Sequence[str], value: Any, decode_values: bool):
+def _assign(root: NestedType, parts: Sequence[str], value: Any, decode_values: bool):  # noqa: C901
     """Recursively assign value into the nested structure based on parts."""
     
     def place_value(container: NestedType, key: str, val: Any):
